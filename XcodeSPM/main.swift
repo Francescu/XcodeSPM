ffdc7ead9f19f01d41ea6da8da7e80c0c97ed2b2
Francescu Santoni <francescu@stupeflix.com>
2016-01-27 20:06:04 +0100

    Initial Commit

---
+++ XcodeSPM/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  XcodeSPM
+//
+//  Created by Francescu Santoni on 27/01/16.
+//  Copyright © 2016 Francescu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
